43eb1b1ee8bbe02c50cd47ed5b201c7369709167
Yonle <yonle@lecturify.net>
2025-06-22 07:48:58 +0700

    use unix instead
    Signed-off-by: Yonle <yonle@lecturify.net>

--- README.md
+++ README.md
@@ -9,18 +9,18 @@ package main
 import (
 	"fmt"
 	"log"
-	"syscall"
 
+	"golang.org/x/sys/unix"
 	"github.com/Yonle/go-epoll"
 )
 
 func main() {
 	// Open hello.txt in non-blocking read-only mode
-	fd, err := syscall.Open("hello.txt", syscall.O_RDONLY|syscall.O_NONBLOCK, 0)
+	fd, err := unix.Open("hello.txt", unix.O_RDONLY|unix.O_NONBLOCK, 0)
 	if err != nil {
 		log.Fatalf("failed to open file: %v", err)
 	}
-	defer syscall.Close(fd)
+	defer unix.Close(fd)
 	fmt.Printf("Opened hello.txt with fd %d\n", fd)
 
 	// Create an epoll instance
@@ -28,16 +28,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to create epoll instance: %v", err)
 	}
-	defer syscall.Close(epi.Fd)
+	defer unix.Close(epi.Fd)
 
 	// Register the file for EPOLLIN events
-	ev := epoll.MakeEvent(fd, syscall.EPOLLIN)
+	ev := epoll.MakeEvent(fd, unix.EPOLLIN)
 	if err := epi.Add(fd, ev); err != nil {
 		log.Fatalf("failed to add fd to epoll: %v", err)
 	}
 
 	// Wait for event
-	events := make([]syscall.EpollEvent, 1)
+	events := make([]unix.EpollEvent, 1)
 	fmt.Println("Waiting for events on hello.txt...")
 
 	n, err := epi.Wait(events, -1)
@@ -46,9 +46,9 @@ func main() {
 	}
 
 	for i := 0; i < n; i++ {
-		if events[i].Fd == int32(fd) && events[i].Events&syscall.EPOLLIN != 0 {
+		if events[i].Fd == int32(fd) && events[i].Events&unix.EPOLLIN != 0 {
 			buf := make([]byte, 1024)
-			n, err := syscall.Read(fd, buf)
+			n, err := unix.Read(fd, buf)
 			if err != nil {
 				log.Fatalf("read error: %v", err)
 			}
--- epoll.go
+++ epoll.go
@@ -1,6 +1,6 @@
 // Package epoll provides an idiomatic Go wrapper around Linux's epoll(7) API.
 //
-// This module simplifies usage of epoll by wrapping syscall.Epoll* functions
+// This module simplifies usage of epoll by wrapping unix.Epoll* functions
 // into a small, clean struct interface. It offers basic operations for creating
 // an epoll instance and managing file descriptors with add, modify, and delete.
 //
@@ -15,7 +15,7 @@
 package epoll
 
 import (
-	"syscall"
+	"golang.org/x/sys/unix"
 )
 
 // The epoll instance
@@ -26,43 +26,43 @@ type Instance struct {
 // Make a new epoll instance
 func NewInstance(flags int) (i *Instance, err error) {
 	i = &Instance{}
-	i.Fd, err = syscall.EpollCreate1(flags)
+	i.Fd, err = unix.EpollCreate1(flags)
 	return
 }
 
 // Add en entry to the interest list of the epoll file descriptor
-func (i *Instance) Add(fd int, ev *syscall.EpollEvent) (err error) {
-	err = i.Ctl(syscall.EPOLL_CTL_ADD, fd, ev)
+func (i *Instance) Add(fd int, ev *unix.EpollEvent) (err error) {
+	err = i.Ctl(unix.EPOLL_CTL_ADD, fd, ev)
 	return
 }
 
-// Change the settings associated with `fd` in the interest list to the new settings specified in `syscall.EpollEvent`
-func (i *Instance) Mod(fd int, ev *syscall.EpollEvent) (err error) {
-	err = i.Ctl(syscall.EPOLL_CTL_MOD, fd, ev)
+// Change the settings associated with [fd] in the interest list to the new settings specified in [unix.EpollEvent]
+func (i *Instance) Mod(fd int, ev *unix.EpollEvent) (err error) {
+	err = i.Ctl(unix.EPOLL_CTL_MOD, fd, ev)
 	return
 }
 
-// Remove (deregister the target file descriptor `fd` from the interest list.
-func (i *Instance) Del(fd int, ev *syscall.EpollEvent) (err error) {
-	err = i.Ctl(syscall.EPOLL_CTL_DEL, fd, ev)
+// Remove (deregister the target file descriptor [fd] from the interest list.
+func (i *Instance) Del(fd int, ev *unix.EpollEvent) (err error) {
+	err = i.Ctl(unix.EPOLL_CTL_DEL, fd, ev)
 	return
 }
 
-// Control interface for an epoll file descriptor from the interest list. The `ev` event argument is ignored and can be `nil`.
-func (i *Instance) Ctl(op, fd int, ev *syscall.EpollEvent) (err error) {
-	err = syscall.EpollCtl(i.Fd, op, fd, ev)
+// Control interface for an epoll file descriptor from the interest list. The [ev] event argument is ignored and can be [nil].
+func (i *Instance) Ctl(op, fd int, ev *unix.EpollEvent) (err error) {
+	err = unix.EpollCtl(i.Fd, op, fd, ev)
 	return
 }
 
 // Wait for an I/O event on an epoll file descriptor
-func (i *Instance) Wait(events []syscall.EpollEvent, timeout int) (n int, err error) {
-	n, err = syscall.EpollWait(i.Fd, events, timeout)
+func (i *Instance) Wait(events []unix.EpollEvent, timeout int) (n int, err error) {
+	n, err = unix.EpollWait(i.Fd, events, timeout)
 	return
 }
 
-// Make an object of EpollEvent
-func MakeEvent(fd int, events uint32) (ev *syscall.EpollEvent) {
-	ev = &syscall.EpollEvent{
+// Make an object of [EpollEvent]
+func MakeEvent(fd int, events uint32) (ev *unix.EpollEvent) {
+	ev = &unix.EpollEvent{
 		Events: events,
 		Fd:     int32(fd),
 	}
